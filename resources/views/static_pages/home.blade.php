d742e9372e6f96d23f58f1c00e4493142bc02ee6
yan <372387016@qq.com>
2017-08-02 17:22:27 +0800

    Finish layout anf routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.default')
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      你现在看到的是 <a href="https://fsdhub.com/books/laravel-essential-training-5.1">Laravel 入门教程</a>的示例项目主页。
+    </p>
+    <p>
+      <a href="{{ route('signup') }}" role="button" class="btn btn-lg btn-success">现在注册</a>
+    </p>
+  </div>
 @stop
